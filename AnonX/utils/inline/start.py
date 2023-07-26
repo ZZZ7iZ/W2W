9b508beddb13c431f2482e1987a338fff89806ad
ZZZ7iZ <131855150+ZZZ7iZ@users.noreply.github.com>
2023-07-26 08:27:44 +0300

    Add files via upload

---
+++ AnonX/utils/inline/start.py
@@ -0,0 +1,59 @@
+from typing import Union
+
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+import config
+from config import SUPPORT_CHANNEL
+
+
+
+def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="‹ : اضفني لمجموعتك : ›",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="‹ : الاوامر : ›",
+                callback_data="settings_back_helper",
+            ),
+            InlineKeyboardButton(
+                text="المساعدة", callback_data="settings_helper"
+            ),
+        ],
+     ]
+    return buttons
+
+
+def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="‹ : اضفني لمجموعتك : ›",
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="‹ : الاوامر : ›", callback_data="settings_back_helper"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="‹ : صنع بوت : ›", url=f"https://t.me/bp_bp"
+            ),
+            InlineKeyboardButton(
+                text="‹ : مطور البوت : ›", user_id=OWNER
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                text="‹ : قناة المطور : ›", url=f"https://t.me/{SUPPORT_CHANNEL}"
+            )
+        ],
+      
+     ]
+    return buttons
